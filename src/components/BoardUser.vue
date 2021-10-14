1e9161f0e1407b68075f8ba03bdc347cae5ce976
markisan31 <mark.kalda94@gmail.com>
2021-10-14 10:33:36 +0300

    first commit

---
+++ src/components/BoardUser.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>{{ content }}</h3>
+    </header>
+  </div>
+</template>
+
+<script>
+import UserService from "../services/user.service";
+import axios from 'axios';
+export default {
+  name: "User",
+  data() {
+    return {
+      content: "",
+    };
+  },
+  mounted() {
+    UserService.getUserBoard().then(
+      (response) => {
+        this.content = response.data;
+        axios
+      .get('http://localhost:8080/user_plants/')
+      .then(response => (this.content = response.data));
+      },
+      (error) => {
+        this.content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+      }
+    );
+  },
+};
+</script>
